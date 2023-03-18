ffceeb21e3c64cde51f08b2b9d755204ccbb594c
caleb <odedejikehinde08@gmail.com>
2023-03-18 12:05:17 -0700

    added image

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import "./components/styles/styles.css"
+import img1 from "./components/images/img1.png"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <img src={img1} className="App-logo" alt="logo" />
         <p>
-          Deployment Done!<br/><span style={{float:"right"}}>- Caleb</span>
+          Deployment Done!
+          <br />
+          <span style={{ float: "right" }}>- Caleb</span>
         </p>
         <a
           className="App-link"
